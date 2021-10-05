77d0323310a0a84fa16a271687f80a8dd22f2132
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2021-10-04 22:54:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//isso é um comentário
+
+/*
+Isso é um bloco
+de comentário
+*/
+
+console.log("Olá Mundo!")
+
+const nomeDousuario= prompt ("Qual é o seu nome?")
+const sobreNome = prompt ("Qual seu sobrenome?")
+const idade = prompt ("Qual é a sua idade?")
+const eEstudante = true 
+ 
+console.log("typeof")
+console.log("Nome de usuario", nomeDousuario)
+
+console.log("sobreNome santos")
+console.log("typeof")
+
+console.log("idade 18")
+
+console.log("estuadante true")
+console.log("typeof")
+
+console.log("Olá nayara, tenho 18 anos")
+
+
