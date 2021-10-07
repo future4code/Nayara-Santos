2226111f1361be981188221bb9143ac6d5c2ffc9
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2021-10-06 22:22:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//isso é um comentário
+
+/*
+Isso é um bloco
+de notas
+
+*/
+
+//let array 
+//console.log('a. ', array)
+
+//array = null
+//console.log('b. ', array)
+
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
+
+//qlet i = 0
+//console.log('d. ', array[i])
+
+//array[i+1] = 19 
+//console.log('e. ', array)
+
+ //const valor = array[i+6]
+ //console.log('f. ', valor)
+
+//const frase = prompt("Digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+// A palavra Marrocos ficara Mirrocos, pois trocou a letra "A" por "I".
+
+const emailDousuario = prompt("Qual é o seu email?")
+const nomeDousuario = prompt("Qual é seu nome?")
+
+const frase = ("O email" + emailDousuario + "foi cadastrado com sucesso.Seja bem-vinda," + nomeDousuario)
+console.log(frase)
+
+const minhasComidaspreferidas = ["lasanha", "panqueca", "yakisoba", "batata-frita", "strogonoff"]
+console.log(minhasComidaspreferidas)
+
+const comidapreferidaDousuario = prompt("Qual é a sua comida preferida?")
+console.log(comidapreferidaDousuario.length)
+
+
+
