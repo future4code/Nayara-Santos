a6245b648adf0c3a81d33b8035f785d1121c44e9
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2021-10-08 13:19:14 -0300

    Revert "Funções"

--- index.js
+++ index.js
@@ -6,103 +6,40 @@ de notas
 
 */
 
-//1°Exercício 
-//a) O que vai ser impresso no console?
-//R:Vai ser impresso os números 10 e 50, pois 5*2=10 e 5*10=50. 
-//b) O que aconteceria se retirasse os dois console.log e simplesmente invocasse a função minhaFuncao(2)
-//e minhaFuncao(10)?O que apareceria no console?
-//R: Iria dar erro ou apareceria undefined.
+//let array 
+//console.log('a. ', array)
 
+//array = null
+//console.log('b. ', array)
 
-//2° Exercício 
-//a) Explique o que essa função faz e qual é sua utilidade.
-//R: Ela é utilizada para executar textos dando os comandos true e false.
-//b) Determine qual será a saída no console para cada uma das 3 entradas do usuário:
-//i. Eu gosto de cenoura
-//ii. CENOURA é bom pra vista
-//iii. Cenouras crescem na terra
-//R: i.R: True, true e false.
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//console.log('c. ', array.length)
 
-//1° Exercício de escrita de código
+//qlet i = 0
+//console.log('d. ', array[i])
 
- function imprimirfrase(frase) {
- console.log(frase.length)
-console.log(frase.toUppeCase)
-}
-imprimirfrase("Eu sou Nayara, tenho 18 anos, moro em São Paulo, sou estudante")
+//array[i+1] = 19 
+//console.log('e. ', array)
 
+ //const valor = array[i+6]
+ //console.log('f. ', valor)
 
-//2°Exercício
+//const frase = prompt("Digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
 
-function somarArea(numero1, numero2) {
-    const area = numero1 + numero2
-console.log(area)
-}
+// A palavra Marrocos ficara Mirrocos, pois trocou a letra "A" por "I".
 
+const emailDousuario = prompt("Qual é o seu email?")
+const nomeDousuario = prompt("Qual é seu nome?")
 
-function imprimirMensagemMaiuscula(mensagem) {
-    console.log("Tamanho da mensagem", mensagem.length)
-    console.log(mensagem.toUpperCase())
-}
-imprimirMensagemMaiuscula("")
+const frase = ("O email" + emailDousuario + "foi cadastrado com sucesso.Seja bem-vinda," + nomeDousuario)
+console.log(frase)
 
+const minhasComidaspreferidas = ["lasanha", "panqueca", "yakisoba", "batata-frita", "strogonoff"]
+console.log(minhasComidaspreferidas)
 
-//3°Exercício
-
-const numero1 = Number(prompt("informe o primeiro número:"))
-const numero2 = Number(prompt("informe o segundo numero"))
-function somar (numero1, numero2) {
-const soma = numero1 + numero2
-    return soma
-}
-
-function subtracao (numero1, numero2) {
-const diferenca = numero1 - numero2
-    return diferenca 
-}
-
-function multiplicacao (numero1, numero2) {
-const multiplicar = numero1 * numero2
- return multiplicar
-}
-
-function dividir (numero1, numero2) {
-const divisao = numero1 / numero2
-  return divisao 
-}
-
-console.log("Os numeros inseridos são", numero1, "e", numero2)
-console.log("Soma:", somar)
-console.log("Diferenca:", subtracao)
-console.log("Multiplicacao:", multiplicacao) 
-console.log("Divisao:", dividir)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const comidapreferidaDousuario = prompt("Qual é a sua comida preferida?")
+console.log(comidapreferidaDousuario.length)
 
 
 
