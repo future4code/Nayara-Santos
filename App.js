261dc090a26c3e8b57874dc1f3005a53350ebc42
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2021-11-08 22:44:27 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Etapa1 from "./components/Etapa1"
+import './App.css';
+
+export default class App extends React.Component {
+
+  mostrarOpcaoDosSelect = (event) => {
+    console.log(event.tanget)
+  }
+
+  render() {
+    return <div>
+      <select onChange = {this.mostrarOpcaoDosSelect}>
+        <option value={"Ensino Médio Incompleto"}>Ensino Médio Incompleto</option>
+        <option value={"Ensino Médio Completo"}>Ensino Médio Completo</option>
+        <option value={"Ensino Superior Incompleto"}>Ensino Superior Incompleto</option>
+        <option value={"Ensino Superior Completo"}>Ensino Superior Completo</option>
+      </select>
+    </div>
+  }
+}
+
+
