296426262af6f3169bae966e674eb68c5189a6ca
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2021-12-05 19:34:02 -0300

    projeto-astromatch

--- modulo3/projeto-astromatch/projeto-astromatch/src/App.js
+++ modulo3/projeto-astromatch/projeto-astromatch/src/App.js
@@ -1,38 +1,92 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import TelaMatch from "./components/TelaMatch";
+import styled from "styled-components"
+
 
 function App() {
-  const [perfil,setPerfil] = useState({})
-  const [proximo, setProximo] = useState({})
+  const [perfil, setPerfil] = useState({})
+  const [proximo, setProximo] = useState(0)
+  const [telaPerfil, setTelaPerfis] = useState(true)
+  const [carregando, setCarregando] = useState("")
+  const [like, setLike] = useState(false)
+ 
   const receberPerfil = () => {
+    setCarregando("carregando")
     axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:nayara-santos-carver/person")
     .then((resposta) => {
       const novoPerfil = resposta.data.profile
       setPerfil(novoPerfil)
+      setCarregando("Carregou")
+    })
+    .catch((erro) => {
+      console.log(erro.response)
+      setCarregando("Deu erro!")
+    })
+    console.log(carregando)
+  }
+
+  useEffect((props) => {
+    receberPerfil()
+  }, [proximo])
+
+  const novoPerfil = () => {
+    setProximo(proximo + 1)
+  }
+  
+  const mudaTela =() => {
+    setTelaPerfis(!telaPerfil)
+  }
+
+  const curtiPerfil = (like) => {
+    setLike(like)
+    enviarLike()
+    novoPerfil()
+  }
+
+  const enviarLike = () => {
+    const body = {
+      id: perfil.id,
+      choice: like
+    }
+    console.log(body)
+    axios.post("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person", body)
+    .then((resposta) => {
+      console.log(resposta)
+      if (resposta.data.isMatch){
+        window.alert("Voce tem um novo match")
+      }
     })
     .catch((erro) => {
       console.log(erro.response)
     })
   }
 
-  useEffect(() => {
-    receberPerfil()
-  }, [proximo])
-
-  const novoPerfil = () => {
-    setProximo(proximo+1)
-  }
-
   return (
     <div>
-      <h1>astromatch</h1>
-      {
-        perfil.id ?
-        <p>{perfil.name}</p>
-        :
-        <p>Não tem nenhum perfil disponivel</p>
+      {telaPerfil ?
+      <div>
+        <h1>AstroMatch</h1>
+      {carregando === "carregando"?
+      <h1>carregando</h1>
+      :<>
+       {
+         perfil.id ?
+         <>
+          <p>{perfil.name}</p>
+          <button onClick={() => curtiPerfil(true)}>Like</button>
+          <button onClick={() => curtiPerfil(false)}>Dislike</button>
+          </>
+          :
+          <p>Não tem nenhum perfil disponivel</p>
+        }</>
       }
       <button onClick={novoPerfil}>Proximo</button>
+    </div>
+    :
+    <TelaMatch perfil={perfil}/>
+    }
+    <button onClick={mudaTela}>Mudar Tela</button>
     </div>
   );
 }
