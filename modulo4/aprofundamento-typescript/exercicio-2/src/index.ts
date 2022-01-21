5e5b1120c3b499bfd680550833da3b5d7f7775ee
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2022-01-20 22:11:02 -0300

    aprofundamento-typescript

---
+++ modulo4/aprofundamento-typescript/exercicio-2/src/index.ts
@@ -0,0 +1,31 @@
+//function obterEstatisticas(numeros) {
+
+    //const numerosOrdenados = numeros.sort(
+        //(a, b) => a - b
+    //)
+
+    //let soma = 0
+
+    //for (let num of numeros) {
+        //soma += num
+    //}
+
+    //const estatisticas = {
+        //maior: numerosOrdenados[numeros.length - 1],
+        //menor: numerosOrdenados[0],
+        //media: soma / numeros.length
+    //}
+
+    //return estatisticas
+//}
+
+//A)R: As entradas são os numerosOrdenados e as saídas são as estatisticas.
+
+type amostraDeIdades = {
+
+    numeros: [21, 18, 65, 44, 15, 18],
+
+    obterEstatisticas: ('numeros')
+
+}
+
