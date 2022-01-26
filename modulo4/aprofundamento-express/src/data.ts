f6bea4f6eda02d64cc7d207c44be105651dc7d6d
Nayara dos Santos Brito <91153389+Nayarasantos18@users.noreply.github.com>
2022-01-25 21:42:51 -0300

    aprofundamento-express

---
+++ modulo4/aprofundamento-express/src/data.ts
@@ -0,0 +1,12 @@
+export const users: any = [
+    {
+        id: 'nayara-santos-carver',
+        afazeres: [
+            {
+                userId: 1,
+                title: 'limpar o banheiro',
+                completed: false
+            }
+        ]
+    }
+]
---
+++ modulo4/aprofundamento-express/src/index.ts
@@ -0,0 +1,37 @@
+import express, {Request, Response} from "express";
+import cors from "express";
+import {users} from "./data"
+
+//const app = express();
+
+//app.use(express.json());
+
+//const server = app.listen(process.env.PORT || 3003, () => {
+    //if (server) {
+      //const address = server.address() as AddressInfo;
+      //console.log(`Server is running in http://localhost:3003/test/hello?name=nayara`);
+    //} else {
+      //console.error(`Failure upon starting server.`);
+    //}
+  //});;
+
+//Exercicio-1
+
+//app.get("/ping", (req, res) => {          
+    //res.send("Pong! 🏓")
+//})
+
+//Exercicio-2
+
+//const app = express()
+
+//app.use(cors())
+//app.use(express.json())
+
+//app.listen(3003, () => console.log("Servidor disponivel em 3003"))
+
+//app.get("/test/hello", (req: Resquest, res: Response) => {
+    //res.send("Olá, mundo!")
+//})
+
+//Exercicio-3
